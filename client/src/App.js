5dda073402ac96d95ea43ad63cbdba3030bf5510
nheske <normheske@gmail.com>
2021-03-27 16:37:32 -0700

    add abort

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,27 @@ const App = () => {
     const [status, setStatus] = useState('Drop Here');
     const [percentage, setPercentage] = useState(0);
     const [preview, setPreview] = useState(null);
+    const [enableDragDrop, setEnableDragDrop] = useState(true);
+    const [stateXhr, setStateXhr] = useState(null);
+
     const onDragEnter = event => {
         console.log(event);
-        setStatus('File Detected');
+        if (enableDragDrop) {
+            setStatus('File Detected');
+        }
         event.preventDefault();
         event.stopPropagation();
     }
     const onDragOver = event => {
-        setStatus('Drop');
+        if (enableDragDrop) {
+            setStatus('Drop');
+        }
         event.preventDefault();
     }
     const onDrop = event => {
         const supportedFilesTypes = ['image/jpeg', 'image/png'];
         const { type } = event.dataTransfer.files[0];
-        if (supportedFilesTypes.indexOf(type) > -1) {
+        if (supportedFilesTypes.indexOf(type) > -1 && enableDragDrop) {
             // Begin Reading File
             const reader = new FileReader();
             reader.onload = e => setPreview(e.target.result);
@@ -34,6 +41,13 @@ const App = () => {
                 const perc = (Math.floor(done/total*1000)/10);
                 if (perc >= 100) {
                     setStatus('Done');
+                    // Delayed reset
+                    setTimeout(() => {
+                        setPreview(null);
+                        setStatus('Drop Here');
+                        setPercentage(0);
+                        setEnableDragDrop(true);
+                    }, 750); // To match the transition 500 / 250
                 } else {
                     setStatus(`${perc}%`);
                 }
@@ -42,13 +56,24 @@ const App = () => {
             // XHR - Make Request  
             xhr.open('POST', 'http://localhost:5000/upload');
             xhr.send(payload);
+            setStateXhr(xhr);
+            setEnableDragDrop(false);
         }
         event.preventDefault();
     };
     const onDragLeave = event => {
-        setStatus('Drop Here');
+        if (enableDragDrop) {
+            setStatus('Drop Here');
+        }
         event.preventDefault();
     }
+        const onAbortClick = () => {
+        stateXhr.abort();
+        setPreview(null);
+        setStatus('Drop Here');
+        setPercentage(0);
+        setEnableDragDrop(true);
+    };
     const doNothing = event => event.preventDefault();
 
     return (
@@ -56,14 +81,16 @@ const App = () => {
             <div className={`ImagePreview ${preview ? 'Show' : ''}`}>
                 <div style={{ backgroundImage: `url(${BgImage})` }} />
             </div>
-            <div className={`DropArea ${status === 'Drop' ? 'Over' : ''}`} onDragOver={onDragOver} onDrop={onDrop} onDragLeave={onDragEnter}>
+            <div className={`DropArea ${status === 'Drop' ? 'Over' : ''} ${status.indexOf('%') > -1 || status === 'Done' ? 'Uploading' : ''}`} onDragOver={onDragOver} onDragLeave={onDragEnter} onDrop={onDrop}>
 
             <div className={`ImageProgress ${preview ? 'Show' : ''}`}>
                  <div className="ImageProgressImage" style={{ backgroundImage: `url(${preview})` }}></div>
                  <div className="ImageProgressUploaded" style={{ backgroundImage: `url(${preview})`, clipPath: `inset(${100 - Number(percentage)}% 0 0 0);` }}></div>
             </div> 
-
-           <div className="Status">{status}</div>
+            <div className={`Status ${status.indexOf('%') > -1 || status === 'Done' ? 'Uploading' : ''}`}>{status}</div>
+            
+            {status.indexOf('%') > -1 && <div className="Abort" onClick={onAbortClick}><span>&times;</span></div>}
+            
             </div>
         </div> 
     );
